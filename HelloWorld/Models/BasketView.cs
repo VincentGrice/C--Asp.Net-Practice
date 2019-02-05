66c383713037c41dbdd60ec15408feb73980014d
Vincent Grice <39641488+vgrice888@users.noreply.github.com>
2019-02-05 16:30:02 -0500

    Add project files.

---
+++ HelloWorld/Models/BasketView.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HelloWorld.Models
+{
+    public class BasketView
+    {
+        public int BasketCount { get; set; }
+        public string BasketTotal { get; set; }
+    }
+}
---
+++ HelloWorld/Models/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace HelloWorld.Models
+{
+    public class Customer
+    {
+        public string Id { get; set; }
+        [Required]
+        [StringLength(10,ErrorMessage ="Your name is too long!")]
+        [DisplayName("Enter your name")]
+        public string Name { get; set; }
+        
+        public string Telephone { get; set; }
+
+    }
+}
